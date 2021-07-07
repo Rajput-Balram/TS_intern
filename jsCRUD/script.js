cd93f547c3288bccc2c78d779dab3112f5c7f4a6
Balram Singh Rajput <55914838+Rajput-Balram@users.noreply.github.com>
2021-07-07 13:11:54 +0530

    some changes

--- jsCRUD/script.js
+++ jsCRUD/script.js
@@ -10,11 +10,11 @@ function onFormSubmit( ) {
 }
 
 function readFormData() {
-    var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
+    var formData = new Object();
+    formData.fullName = document.getElementById("fullName").value;
+    formData.empCode = document.getElementById("empCode").value;
+    formData.salary = document.getElementById("salary").value;
+    formData.city = document.getElementById("city").value;
     return formData;
 }
 
